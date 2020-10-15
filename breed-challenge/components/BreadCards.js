3374446811f60f76f23fc327acf9349ab1ca6b1f
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-10-15 15:44:25 -0700

    wip styled cards

--- breed-challenge/components/BreadCards.js
+++ breed-challenge/components/BreadCards.js
@@ -1,17 +1,18 @@
 import React from "react";
+import styles from './breedcards.module.css';
 
 export default function BreedCards(props) {
   const images = props.images.message;
   const breed = props.alt;
 
   return (
-    <div>
+    <div className={styles.cardContainer}>
       {images.map((img) => (
-        <div key={Math.random()}>
-          <div>
-            <img src={img} alt={breed} />
+        <div className={styles.card} key={Math.random()}>
+          <div className={styles.imgContainer}>
+            <img className={styles.img} src={img} alt={breed} />
           </div>
-          <div>
+          <div className={styles.breedInfo}>
             <span>I am a {breed.toUpperCase()}</span>
           </div>
         </div>
--- breed-challenge/components/Layout.js
+++ breed-challenge/components/Layout.js
@@ -3,18 +3,30 @@ import style from "../components/layout.module.css";
 import Link from "next/link";
 
 export default function Layout({ children }) {
-  let breeds = ['affenpinscher', 'boxer', 'cotondetulear', 'havanese', 'malamute', 'pug']
+  let breeds = [
+    "affenpinscher",
+    "boxer",
+    "cotondetulear",
+    "havanese",
+    "malamute",
+    "pug",
+  ];
   return (
     <div className="container">
       <nav className={style.nav}>
         <ul className={style.navList}>
-        {breeds.map(breed => (
-          <Link key={breed} href={`/breeds/[breed]`} as={`/breeds/${breed}`}>
-          <a>
-          <li className={style.navItem}>{breed.toUpperCase()}</li>
-          </a>
+          <Link href={`/`}>
+            <a>
+              <li className={style.navItem}>HOME</li>
+            </a>
           </Link>
-        ))}
+          {breeds.map((breed) => (
+            <Link key={breed} href={`/breeds/[breed]`} as={`/breeds/${breed}`}>
+              <a>
+                <li className={style.navItem}>{breed.toUpperCase()}</li>
+              </a>
+            </Link>
+          ))}
         </ul>
       </nav>
       <main>{children}</main>
